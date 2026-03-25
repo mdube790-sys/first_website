c584bd4883f7e68197d4ff29688afe60fe04a866
mdube790-sys <mdube790@gmail.com>
2026-03-25 11:19:11 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let name = "Musa";
+let age = 30;
+let isStudent = false;
+console.log("Name:" + name);
+console.log("Age:" + age);
+console.log("Is Student:" isStudent); 
+
+function add task(){
+    let task = document.get Elementby ld("task").value;
+    let li = document.createElement("li")
+    li.TestContet = task;
+    document.getElementbyld("list").appendchild(li);
+}
